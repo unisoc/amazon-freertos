a2146831dc7be3a7ce6048f33b3c2cd4082424a0
shilei.yu <shilei.yu@unisoc.com>
2019-03-04 15:40:32 +0800

    set newlib-nano as default standard C library

---
+++ tests/unisoc/ivy5661/common/application_code/vendor_code/bsp/syscalls.c
@@ -0,0 +1,241 @@
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+extern uint32_t __sbrk_start;
+extern uint32_t __krbs_start;
+
+caddr_t _sbrk(int incr)
+{
+    static uint32_t heap_ind = (uint32_t)(&__sbrk_start);
+    uint32_t heap_ind_pre = heap_ind;
+    uint32_t heap_ind_new = (heap_ind_pre + incr + 0x07) & ~0x07;
+    if (heap_ind_new > (uint32_t)(&__krbs_start)) {
+        errno = ENOMEM;
+        return (caddr_t)(-1);
+    }
+    heap_ind = heap_ind_new;
+    return (caddr_t) heap_ind_pre;
+}
+
+#include "uwp_uart.h"
+extern serial_t stdio_uart;
+int _write(int file, char *ptr, int len)
+{
+	int i;
+	if(file < 3){
+		serial_init(&stdio_uart, NC, NC);
+		for(i = 0; i < len; i++)
+			serial_putc(&stdio_uart, ptr[i]);
+	}
+	return len;
+}
+
+int _read (int file, char *ptr, int len)
+{
+	/* The I/O library uses an internal buffer */
+	/* It asks for 1024 characters even if only getc() is used. */
+	/* If we use a for(;;) loop on the number of characters requested, */
+	/* the user is forced to enter the exact number requested, even if only one is needed. */
+	/* So here we return only 1 character even if requested length is > 1 */
+	//*ptr = __io_getchar();
+
+	return 1;
+}
+
+int _close(int file)
+{
+	return -1;
+}
+
+
+int _fstat(int file, struct stat *st)
+{
+	//st->st_mode = S_IFCHR;
+	return 0;
+}
+
+int _isatty(int file)
+{
+	return 1;
+}
+
+int _lseek(int file, int ptr, int dir)
+{
+	return 0;
+}
+
+#if 0
+/* Includes */
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+
+/* Variables */
+//#undef errno
+extern int errno;
+#define FreeRTOS
+#define MAX_STACK_SIZE 0x2000
+
+extern int __io_putchar(int ch) __attribute__((weak));
+extern int __io_getchar(void) __attribute__((weak));
+
+#ifndef FreeRTOS
+  register char * stack_ptr asm("sp");
+#endif
+
+
+register char * stack_ptr asm("sp");
+
+char *__env[1] = { 0 };
+char **environ = __env;
+
+
+/* Functions */
+void initialise_monitor_handles()
+{
+}
+
+int _getpid(void)
+{
+	return 1;
+}
+
+int _kill(int pid, int sig)
+{
+	errno = EINVAL;
+	return -1;
+}
+
+void _exit (int status)
+{
+	_kill(status, -1);
+	while (1) {}		/* Make sure we hang here */
+}
+
+int _read (int file, char *ptr, int len)
+{
+	/* The I/O library uses an internal buffer */
+	/* It asks for 1024 characters even if only getc() is used. */
+	/* If we use a for(;;) loop on the number of characters requested, */
+	/* the user is forced to enter the exact number requested, even if only one is needed. */
+	/* So here we return only 1 character even if requested length is > 1 */
+	*ptr = __io_getchar();
+
+	return 1;
+}
+
+int _write(int file, char *ptr, int len)
+{
+	int DataIdx;
+
+	for (DataIdx = 0; DataIdx < len; DataIdx++)
+	{
+		__io_putchar(*ptr++);
+	}
+	return len;
+}
+
+caddr_t _sbrk(int incr)
+{
+	extern char end asm("end");
+	static char *heap_end;
+	char *prev_heap_end;
+
+	if (heap_end == 0)
+		heap_end = &end;
+
+	prev_heap_end = heap_end;
+	if (heap_end + incr > stack_ptr)
+	{
+//		write(1, "Heap and stack collision\n", 25);
+//		abort();
+		errno = ENOMEM;
+		return (caddr_t) -1;
+	}
+
+	heap_end += incr;
+
+	return (caddr_t) prev_heap_end;
+}
+
+int _close(int file)
+{
+	return -1;
+}
+
+
+int _fstat(int file, struct stat *st)
+{
+	st->st_mode = S_IFCHR;
+	return 0;
+}
+
+int _isatty(int file)
+{
+	return 1;
+}
+
+int _lseek(int file, int ptr, int dir)
+{
+	return 0;
+}
+
+int _open(char *path, int flags, ...)
+{
+	/* Pretend like we always fail */
+	return -1;
+}
+
+int _wait(int *status)
+{
+	errno = ECHILD;
+	return -1;
+}
+
+int _unlink(char *name)
+{
+	errno = ENOENT;
+	return -1;
+}
+
+int _times(struct tms *buf)
+{
+	return -1;
+}
+
+int _stat(char *file, struct stat *st)
+{
+	st->st_mode = S_IFCHR;
+	return 0;
+}
+
+int _link(char *old, char *new)
+{
+	errno = EMLINK;
+	return -1;
+}
+
+int _fork(void)
+{
+	errno = EAGAIN;
+	return -1;
+}
+
+int _execve(char *name, char **argv, char **env)
+{
+	errno = ENOMEM;
+	return -1;
+}
+#endif
